04ec6211ad3436badcdfbbc6c556f50ca4ceea64
Raiza Santana <raiza.santanna@gmail.com>
2015-07-24 17:56:47 -0300

    Alterando o DBTest.

--- src/br/ccomp/testes/testesDBUnit/ConsumidorDBTest.java
+++ src/br/ccomp/testes/testesDBUnit/ConsumidorDBTest.java
@@ -1,13 +1,18 @@
-package br.ccomp.dbUnitTests;
+package br.ccomp.testes.testesDBUnit;
 
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.junit.Test;
 
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
 import com.sun.org.apache.xpath.internal.functions.FuncBoolean;
 
+import br.ccomp.gateway.ConnectionFactory;
 import br.ccomp.gateway.ConsumidorGateway;
 import br.ccomp.modelo.Aluno;
 import br.ccomp.modelo.Consumidor;
@@ -17,70 +22,150 @@ import br.ccomp.modelo.Titulo;
 
 public class ConsumidorDBTest {
 
-	private static final Object MESTRADO = null;
 	private static final Object FEMININO = null;
 	private static final Sexo MASCULINO = null;
+	private static final Object MESTRADO = null;
 	private ConsumidorGateway consGtw;
-	
-	public ConsumidorDBTest(){
-		
+
+	public ConsumidorDBTest() {
+
 		consGtw = new ConsumidorGateway();
-		
+
 	}
-	
-	
-	
+
 	@Test
-	public void testInsertFuncionario() throws SQLException{
+	public void testInsertFuncionario() throws SQLException {
 		Funcionario f = new Funcionario(1000, 1000);
-		consGtw.insertFuncionario(1, "", "90891775315", "MESTRADO", 1000, 2000, "");
+
+		consGtw.insertFuncionario(1, "Roberta", "90891775315", "MESTRADO",
+				1000, 2000, "FEMININO");
+
+		Connection con = ConnectionFactory.getConnection();
+		PreparedStatement prst = (PreparedStatement) con
+				.prepareStatement("select * from CONSUMIDOR where cpf = '90891775315'");
+
+		// Pega o valor atual
+		ResultSet rst = prst.executeQuery();
+
+		if (rst.next()) {
+			assertEquals("MESTRADO", rst.getString("titulo"));
+			prst.close();
+		}
+	}
+
+	@Test
+	public void testInsertAluno() throws SQLException {
+
+		consGtw.insertAluno(1, "Alex", "78928087449", 20000, 2000, "MASCULINO");
+
+		Connection con = ConnectionFactory.getConnection();
+		PreparedStatement prst = (PreparedStatement) con
+				.prepareStatement("select * from CONSUMIDOR where cpf = '78928087449'");
+
+		// Pega o valor atual
+		ResultSet rst = prst.executeQuery();
+
+		if (rst.next()) {
+			assertEquals("78928087449", rst.getString("cpf"));
+			prst.close();
+		}
+	}
+
+	@Test
+	public void testGetTitulo() throws SQLException {
+
+		Connection con = ConnectionFactory.getConnection();
+		PreparedStatement prst = (PreparedStatement) con
+				.prepareStatement("select * from CONSUMIDOR where cpf = '90891775315'");
+
+		// Pega o valor atual
+		ResultSet rst = prst.executeQuery();
+
+		if (rst.next()) {
+			int id = rst.getInt("id");
+			assertEquals("MESTRADO", consGtw.find(id).getTitulo().getNome());
+		}
+	}
+
+	@Test
+	public void testGetSexo() throws SQLException {
 		
-		assertEquals(true, consGtw.findbyCpf("90891775315"));
-	}
-	
-	@Test
-	public void testInsertAluno() throws SQLException{
-		Aluno a = new Aluno(2000, 20000);
-		consGtw.insertAluno(1, "", "78928087449", 20000, 2000, "");
-		
-		assertEquals(true, consGtw.findbyCpf("78928087449"));
-	}
-	
-	@Test
-	public void testGetTitulo() throws SQLException{
-		assertEquals(MESTRADO, consGtw.findByMatricula(345).getTitulo());
-	}
-	
-	@Test
-	public void testGetSexo() throws SQLException
-	{
-		assertEquals(FEMININO, consGtw.findByMatricula(1234).getSexo());
-	}
-	
-	@Test
-	public void testFindByMatricula() throws SQLException
-	{
-		assertEquals((Integer)1, consGtw.findByMatricula(1234).getId());
-	}
-	
-	@Test
-	public void testFind() throws SQLException
-	{
-		assertEquals("124355534", consGtw.find(1).getCpf());
-	}
-	
-	@Test
-	public void testFindByCpf() throws SQLException
-	{
-		assertEquals(true, consGtw.findbyCpf("124355534"));
-	}
-	
-	@Test
-	public void testUpdate() throws SQLException
-	{
-		consGtw.update(2, 2016, 345, "MAISA", "FEMININO");
-		assertEquals(2016, consGtw.find(2).getAnoIngresso());
+		Connection con = ConnectionFactory.getConnection();
+		PreparedStatement prst = (PreparedStatement) con
+				.prepareStatement("select * from CONSUMIDOR where cpf = '90891775315'");
+
+		// Pega o valor atual
+		ResultSet rst = prst.executeQuery();
+
+		if (rst.next()) {
+			int id = rst.getInt("id");
+			assertEquals(FEMININO, consGtw.find(id).getSexo());
+		}
 		
 	}
-	
+
+	@Test
+	public void testFindByMatricula() throws SQLException {
+		Connection con = ConnectionFactory.getConnection();
+		PreparedStatement prst = (PreparedStatement) con
+				.prepareStatement("select * from CONSUMIDOR where cpf = '90891775315'");
+
+		// Pega o valor atual
+		ResultSet rst = prst.executeQuery();
+
+		if (rst.next()) {
+			int matricula = rst.getInt("matricula");
+			
+			assertEquals("90891775315", consGtw.findByMatricula(matricula).getCpf());
+		}
+	}
+
+	@Test
+	public void testFind() throws SQLException {
+		Connection con = ConnectionFactory.getConnection();
+		PreparedStatement prst = (PreparedStatement) con
+				.prepareStatement("select * from CONSUMIDOR where cpf = '90891775315'");
+
+		// Pega o valor atual
+		ResultSet rst = prst.executeQuery();
+
+		if (rst.next()) {
+			int id = rst.getInt("id");
+			assertEquals("90891775315", consGtw.find(id).getCpf());
+		}
+	}
+
+	@Test
+	public void testFindByCpf() throws SQLException {
+		Connection con = ConnectionFactory.getConnection();
+		PreparedStatement prst = (PreparedStatement) con
+				.prepareStatement("select * from CONSUMIDOR where cpf = '90891775315'");
+
+		// Pega o valor atual
+		ResultSet rst = prst.executeQuery();
+
+		if (rst.next()) {
+			String cpf = rst.getString("cpf");
+			assertEquals(true, consGtw.findbyCpf(cpf));
+		}
+	}
+
+	@Test
+	public void testUpdate() throws SQLException {
+		
+		consGtw.update(1, 2016, 345, "MAISA", "FEMININO");
+		
+		Connection con = ConnectionFactory.getConnection();
+		PreparedStatement prst = (PreparedStatement) con
+				.prepareStatement("select * from CONSUMIDOR where id = 1");
+
+		// Pega o valor atual
+		ResultSet rst = prst.executeQuery();
+
+		if (rst.next()) {
+		int anoIngresso = rst.getInt("ano_ingresso");	
+		assertEquals(2016, consGtw.find(1).getAnoIngresso());
+		}
+	}
+
 }
